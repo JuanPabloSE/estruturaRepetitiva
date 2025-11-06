1a5b2086528115620969e7ff3d9d29ee5ca09eb4
JuanPabloSE <juancontas@protonmail.com>
2025-11-06 11:49:25 -0300

    Exércicio Estrutura Repetitiva

---
+++ src/beecrowd1154.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class beecrowd1154 {
+    public static void main (String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int idade = sc.nextInt();
+        int soma = 0;
+        int contador = 0;
+
+        while (idade >= 0){
+            soma += idade;
+            contador++; // CONTADOR + 1
+            idade = sc.nextInt();
+        }
+
+        if (contador > 0){
+            double media = (double) soma / contador;
+            System.out.printf("%.2f\n", media);
+        }
+
+        sc.close();
+    }
+}
