edd64627771cc77c9e7ad4618bb5b0121db008d2
JuanPabloSE <juancontas@protonmail.com>
2025-11-26 18:00:15 -0300

    Exércicio Estrutura Repetitiva

---
+++ src/Exemp11.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exemp11 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        double divisao;
+
+        for (int i = 0; i < N; i++){
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+
+            if (Y == 0){
+                System.out.println("Divisao impossivel");
+            }
+
+            else {
+                divisao = (double) X / Y;
+                System.out.printf("%.2f\n", divisao);
+            }
+
+        }
+
+
+
+
+
+        sc.close();
+    }
+}
